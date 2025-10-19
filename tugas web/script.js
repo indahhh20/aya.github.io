fe15f51ac3fef2ac2f641edeb261592656b838aa
indahhh20 <indahnili611@gmail.com>
2025-10-19 23:57:35 +0800

    Create script.js

---
+++ tugas web/script.js
@@ -0,0 +1,7 @@
+// Ambil elemen iframe
+const frameTugas = document.getElementById('frame-tugas');
+
+// Fungsi untuk mengganti sumber (src) dari iframe
+function tampilkanTugas(path) {
+    frameTugas.src = path;
+}
